5ace0ffae01d9b3a2553942c882ca20ca836332b
Francois Marier <francois@brave.com>
2022-08-23 18:36:33 -0700

    Extend test harness for new JSON list

--- tools/debounce_validation/test_debounce_validation.py
+++ tools/debounce_validation/test_debounce_validation.py
@@ -11,9 +11,13 @@ NEVERMATCHES = "https://this-never-matches-but-should-never-throw.com"
 
 class DebounceTester(unittest.TestCase):
     def setUp(self):
+        self.rules_debounce = self.loadRules('debounce.json')
+        self.rules_cleanurls = self.loadRules('clean-urls.json')
+
+    def loadRules(self, filename):
         basedir = os.path.dirname(__file__)
         debounce_json = os.path.join(basedir, "..", "..",
-                                     "brave-lists", "debounce.json")
+                                     "brave-lists", filename)
         with open(debounce_json) as fd:
             debounce_raw = json.load(fd)
 
@@ -21,11 +25,16 @@ class DebounceTester(unittest.TestCase):
         for ruleset in debounce_raw:
             rules.extend(ruleset['include'])
             rules.extend(ruleset['exclude'])
-        self.rules = rules
+        return rules
 
     def test_load_debounce_list_no_exceptions(self):
         self.assertFalse(
             any(
-                (urlmatch(u, NEVERMATCHES) for u in self.rules)
+                (urlmatch(u, NEVERMATCHES) for u in self.rules_debounce)
+            )
+        )
+        self.assertFalse(
+            any(
+                (urlmatch(u, NEVERMATCHES) for u in self.rules_cleanurls)
             )
         )
