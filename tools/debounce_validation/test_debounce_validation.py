05bf17c8a254f829321f33531d51e662d9f88dff
Francois Marier <francois@brave.com>
2024-07-02 16:33:23 -0700

    Add new config file for extending "copy clean link" to the JS API (fixes #1852)
    This is the supporting file for brave/brave-browser#33037.

--- tools/debounce_validation/test_debounce_validation.py
+++ tools/debounce_validation/test_debounce_validation.py
@@ -11,18 +11,24 @@ NEVERMATCHES = "https://this-never-matches-but-should-never-throw.com"
 
 class DebounceTester(unittest.TestCase):
     def setUp(self):
-        self.rules_debounce = self.loadRules('debounce.json')
-        self.rules_cleanurls = self.loadRules('clean-urls.json')
+        self.rules_debounce = self.loadRules(self.readFile('debounce.json'))
+        self.rules_cleanurls = self.loadRules(self.readFile('clean-urls.json'))
 
-    def loadRules(self, filename):
+        js_api_rules = self.readFile('clean-urls-permissions.json')
+        self.rules_cleanurlperms = js_api_rules['js-api']
+
+    def readFile(self, filename):
         basedir = os.path.dirname(__file__)
         debounce_json = os.path.join(basedir, "..", "..",
                                      "brave-lists", filename)
         with open(debounce_json) as fd:
             debounce_raw = json.load(fd)
 
+        return debounce_raw
+
+    def loadRules(self, raw_rules):
         rules = []
-        for ruleset in debounce_raw:
+        for ruleset in raw_rules:
             rules.extend(ruleset['include'])
             rules.extend(ruleset['exclude'])
         return rules
@@ -36,5 +42,10 @@ class DebounceTester(unittest.TestCase):
         self.assertFalse(
             any(
                 (urlmatch(u, NEVERMATCHES) for u in self.rules_cleanurls)
+            )
+        )
+        self.assertFalse(
+            any(
+                (urlmatch(u, NEVERMATCHES) for u in self.rules_cleanurlperms)
             )
         )
